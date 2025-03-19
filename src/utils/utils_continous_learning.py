d68dec1549f4fced5dbf897edb71ac39cd01e837
LIGHTER91 <lucien.lachaud@etu.u-bordeaux.fr>
2025-03-19 18:44:17 +0100

    test result cl

--- src/utils/utils_continous_learning.py
+++ src/utils/utils_continous_learning.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 import torch
 import shutil
 import os
-
+import csv
 def continual_learning_yolo(model_path, data_config_path, epochs=10, img_size=640, batch_size=16, save_path='backup_model.pt',directory='', progress_callback=None):
     """
-    Effectue l'apprentissage continu sur un modèle YOLOv8 pré-entraîné avec de nouvelles données.
+    Effectue l'apprentissage continu sur un modèle YOLOv12n pré-entraîné avec de nouvelles données.
 
-    :param model_path: Chemin vers le modèle YOLOv8 pré-entraîné.
+    :param model_path: Chemin vers le modèle YOLOv12n pré-entraîné.
     :param data_config_path: Chemin vers le fichier de configuration des nouvelles données.
     :param epochs: Nombre d'époques d'entraînement.
     :param img_size: Taille des images d'entrée.
@@ -42,12 +42,29 @@ def continual_learning_yolo(model_path, data_config_path, epochs=10, img_size=64
     print(best_model_path)
     if os.path.exists(best_model_path):
         os.rename(best_model_path, save_path)
-
+        # Validate the model on the test dataset
+    metrics = model.val(data=data_config_path, imgsz=img_size, conf=0.25, iou=0.45, split='test', device=device)
     # Conserver results.png
     results_png_path = os.path.join(directory_save_path, 'train', 'results.png')
     print(results_png_path)
     if os.path.exists(results_png_path):
         os.makedirs(os.path.join(directory, 'results'), exist_ok=True)
         os.rename(results_png_path, os.path.join(directory, 'results',f'results_{file_name_without_extension}.png'))
+    # Enregistrer les métriques dans un fichier CSV
+    results_csv_path = os.path.join(directory, 'results', f'results_test_{file_name_without_extension}.csv')
+    os.makedirs(os.path.dirname(results_csv_path), exist_ok=True)
 
+    with open(results_csv_path, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["Metric", "Value"])  # En-tête
 
+        # Ajouter les métriques principales
+        writer.writerow(["mAP50-95", metrics.box.map])
+        writer.writerow(["mAP50", metrics.box.map50])
+        writer.writerow(["mAP75", metrics.box.map75])
+
+        # Ajouter les mAP par catégorie
+        for idx, map_value in enumerate(metrics.box.maps):
+            writer.writerow([f"mAP50-95 catégorie {idx}", map_value])
+
+    print(f"Métriques enregistrées dans {results_csv_path}")
